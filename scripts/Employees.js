36c7c29d67d860b65da51ddd578c13dc5e07e138
Jason Ellison <jason.w.ellison@gmail.com>
2022-07-11 10:57:29 -0400

    Added alert to display number of products sold on click

--- scripts/Employees.js
+++ scripts/Employees.js
@@ -1,6 +1,36 @@
-import { getEmployees } from "./database.js"
+import { getEmployees, getOrders } from "./database.js"
 
 const employees = getEmployees()
+const orders = getOrders()
+
+const getEmployeeProductSoldCount = (employee) => {
+    
+    let productSoldCount = 0
+
+    for ( const order of getOrders() ) {        // getOrders returns a copy of the orders array. A reference would suffice.
+        if ( order.employeeId === employee.id ) productSoldCount++
+    }
+
+    return productSoldCount
+
+}
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if (itemClicked.id.startsWith("employee")) {
+            const [,employeeId] = itemClicked.id.split("--")
+
+            for (const employee of employees) {
+                if (employee.id === parseInt(employeeId)) {
+                    window.alert(`${employee.name} sold ${getEmployeeProductSoldCount(employee)} products.`)
+                }
+            }
+        }
+    }
+)
+
 
 export const Employees = () => {
     let html = "<ul>"
