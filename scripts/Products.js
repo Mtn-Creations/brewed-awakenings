bb943bbc0012691dc3606ef6fabd12701ac716fc
Jason Ellison <jason.w.ellison@gmail.com>
2022-07-11 09:30:27 -0400

    Added alert to display product pricing on click

--- scripts/Products.js
+++ scripts/Products.js
@@ -2,6 +2,7 @@ import { getProducts } from "./database.js"
 
 const products = getProducts()
 
+
 export const Products = () => {
     let html = "<ul>"
 
@@ -14,3 +15,19 @@ export const Products = () => {
     return html
 }
 
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if (itemClicked.id.startsWith("product")) {
+            const [,productId] = itemClicked.id.split("--")
+
+            for (const product of products) {
+                if (product.id === parseInt(productId)) {
+                    window.alert(`${product.name} costs \n$${product.price.toFixed(2)}`)
+                }
+            }
+        }
+    }
+)
